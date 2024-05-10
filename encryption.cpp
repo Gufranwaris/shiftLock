0f93d69f7455e2418c9e51fb1fd558da20775704
Gufran <gufranwaris6098@gmail.com>
2024-05-10 20:11:29 +0530

    final changes

---
+++ encryption.cpp
@@ -0,0 +1,53 @@
+#include "ecnryption.h"
+#include <fstream>
+#include <cctype>
+
+using namespace std;
+
+bool performCaeserCipher(string &content, bool encrypt)
+{
+    int shift = encrypt ? 3 : -3;
+
+    for (char &ch : content)
+    {
+        if (isalpha(ch))
+        {
+            char base = isupper(ch) ? 'A' : 'a';
+            ch = static_cast<char>((ch - base + shift + 26) % 26 + base);
+        }
+    }
+    return true;
+}
+
+bool encryptFile(const string &filename, bool encrypt)
+{
+
+    // open the input file
+    ifstream inFile(filename);
+
+    // Check if the file is open
+    if (!inFile)
+    {
+        return false;
+    }
+
+    // Read the content of the file
+    string content((istreambuf_iterator<char>(inFile)), {});
+    inFile.close();
+
+    if (performCaeserCipher(content, encrypt))
+    {
+        // Create an output file and writing the modified content
+        ofstream outFile(encrypt ? "encrypted_" + filename : "decrypted_" + filename);
+        if (!outFile)
+        {
+            return false;
+        }
+
+        outFile << content;
+
+        outFile.close();
+
+        return true;
+    }
+}
